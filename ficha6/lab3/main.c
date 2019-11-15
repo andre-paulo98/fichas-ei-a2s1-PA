7300090e183b9bf91e6d0c1aa72f9f26dcaa33be
André Paulo <andre_duivenvoorden@hotmail.com>
2019-11-15 09:12:30 +0000

    labs, ficha 6

---
+++ ficha6/lab3/main.c
@@ -0,0 +1,36 @@
+/**
+* @file main.c
+* @brief Description
+* @date 2018-1-1
+* @author name of author
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <assert.h>
+
+#include "debug.h"
+#include "memory.h"
+
+int main(int argc, char *argv[]){
+	/* Disable warnings */
+	(void)argc; (void)argv;
+
+    /* Mostrar tamanho (em bytes) do tipo struct sockaddr_in */
+
+    /* Colocar a "zero" uma variável (my_addr_IPv4) do tipo struct sockaddr_in */
+
+    /* Mostrar bytes da memória da variável my_addr_IPv4 (criar e chamar função)*/
+
+    /* Colocar a "4" os primeiros 6 bytes da variável my_addr_IPv4 */
+
+    /* Mostrar bytes da memória da variável my_addr_IPv4 (chamar função)*/
+
+	return 0;
+}
+
